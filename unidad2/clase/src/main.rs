52093542f253b81922f018ea4b03dec1499e7a3a
Diego Cortes <dfer.cortes@gmail.com>
2025-07-01 23:43:44 -0500

    agregando ejemplos de clase unidad 2 parte 2

--- unidad2/clase/src/main.rs
+++ unidad2/clase/src/main.rs
@@ -1,3 +1,5 @@
+use std::error;
+
 fn main() {
         let edad_a: i32 = 28;
         let edad_b: u16 = 30;
@@ -47,7 +49,98 @@ fn main() {
         println!("x: {}", tupla.0);
         println!("y: {}", tupla.1);
         println!("z: {}", tupla.2);
+
+        /////////////////////////////////////
+         
+        let x = 5;
+        let y = 6;
+        
+        let resultado = sumar(x, y);
+        
+        println!("resultado: {}", resultado);
         
         
+        /////////////////////////////////////////
         
+        //let es_verdadero = false;
+        //let resultado = if es_verdadero { 10 } else { "dos" };
+        
+        /////////////////////////////////////////
+        
+        let x = 4;
+        
+        match x {
+                0..=5 => println!("0 a 5"),
+                6..10 => println!("6 a 9"),
+                _ => println!("otro valor"),
+        }
+        
+        /////////////////////////////////////
+        
+        let letra = 'n';
+        
+        match letra {
+                'a' =>  println!("vocal"),
+                'e' =>  println!("vocal"),
+                'i' =>  println!("vocal"),
+                'o' =>  println!("vocal"),
+                'u' =>  println!("vocal"),
+                _ => println!("consonante"),
+        }
+        
+        ////////////////////////////////////
+        
+        let coordenadas = (3.15, 9.1);
+        
+        // ¿donde estoy?
+        match coordenadas {
+                (0.0, 0.0) => println!("origen"),
+                (x, 0.0) => if x > 0.0 {
+                                        println!("Hemisferio Norte x: {}", x)
+                                 } else {
+                                        println!("Hemisferio Sur x: {}", x)
+                                 },
+                (0.0, y) => if y > 0.0 {
+                                        println!("Este: y {}", y)
+                                } else {
+                                        println!("Oeste: y {}", y)
+                                },
+                (x, y) => if x > 0.0 && y > 0.0 {
+                                        println!("Norte y Este: x {}, y {}", x, y)
+                                   } else if x > 0.0 && y < 0.0 {
+                                        println!("Norte y Oeste: x {}, y {}", x, y)
+                                   } else if x < 0.0 && y > 0.0 {
+                                        println!("Sur y Este: x {}, y {}", x, y)
+                                        }
+        
+        }
+                
+        
+       ////////////////////////////////////////////////////////
+        
+        for item in 0..10 {
+                if item == 5 {
+                        println!("salta {}", item);
+                        continue;
+                }
+                
+                println!("item: {}", item);
+        }
+        
+        ///////////////////////////////////////////////
+        
+        let mut number = 0;
+        
+         let valor_final = loop {
+                //println!("{}", number);
+                if number == 10 {
+                        break "hola loop"
+                }
+                number += 1;                
+        };
+        println!("valor final: {}", valor_final);
+}
+
+fn sumar(parametro1: i32, parametro2: i32) -> i32 { 
+        parametro1 + parametro2
 }
